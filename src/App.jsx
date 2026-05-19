31fac3ddef78286611dbc485bcc2c355757ef346
unknown <nguyentiendat102005@gmail.com>
2026-05-19 22:24:54 +0700

    config_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import HomeIcon from '@mui/icons-material/Home';
-import { pink } from '@mui/material/colors';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import  Typography  from '@mui/material/Typography'
+
 function App() {
   return (
     <>
     <div>dazoriii</div>
+
+    <Typography variant="h1" color="text.secondary">
+      h1. Heading
+    </Typography>
+
      <Button variant="text">Text</Button>
-     <Button variant="contained">Contained</Button>
+     <Button variant="contained" color= "success">Contained</Button>
      <Button variant="outlined">Outlined</Button>
      <br />
      <AccessAlarmIcon />
